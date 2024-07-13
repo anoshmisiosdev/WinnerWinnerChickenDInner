cef48f1ca2ff3d6e5fab053d0c886996d395d166
Aditya Kolekar <128189377+HeadlessSnow786@users.noreply.github.com>
2024-07-12 18:45:01 -0700

    Fixed run error

--- src/main.py
+++ src/main.py
@@ -10,26 +10,48 @@
 # Library imports
 from vex import *
 
-    # Brain should be defined by default
-brain=Brain()
-controller=Controller()
+# Brain should be defined by default
+brain = Brain()
+
+controller = Controller()
 brain.screen.print("Hello V5")
+
 motor1 = Motor(Ports.PORT1, False)  # Front left, direction reversed
 motor2 = Motor(Ports.PORT2, True)  # Front right
 motor3 = Motor(Ports.PORT3, False) # Back left, direction reversed
 motor4 = Motor(Ports.PORT4, True) # Back right
 motor5 = Motor(Ports.PORT5, False)
 motor6 = Motor(Ports.PORT6, True)
+
 motorgroupleft = MotorGroup(motor1, motor3)
 motorgroupright = MotorGroup(motor2, motor4)
 drivemode = DriveTrain(motorgroupleft, motorgroupright, 319.19, 295, 40, MM, 1)
 escalator = Motor(Ports.PORT5, True)
+
+slow_mode = controller.buttonL1
 #bing bong
+
 def autonomous():
     return
+
+def check_controller():
+    while True:
+        # left motor group spin
+        if controller.axis3.position() > 0:
+            motorgroupleft.spin(FORWARD, controller.axis3.position, PERCENT)
+
+        # right motor group spin
+        if controller.axis2.position() > 0:
+            motorgroupright.spin(FORWARD, controller.axis3.position, PERCENT)
+        
+
+
+
+
 def drive():
-    motorgroupleft.spin(FORWARD, controller.axis3.value)
-    motorgroupright.spin(FORWARD, controller.axis2.value)
+    check_controller()
+
+
 def user_control():
     while True:
         drive()
