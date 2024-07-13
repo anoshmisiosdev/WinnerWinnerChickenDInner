4cb0b86ea0349cc8f09a1febb9da19ed09c754dc
Riyan Anosh <riyananosh@gmail.com>
2024-07-12 18:03:13 -0700

    test drive mode

--- src/main.py
+++ src/main.py
@@ -20,30 +20,16 @@ motor3 = Motor(Ports.PORT3, False) # Back left, direction reversed
 motor4 = Motor(Ports.PORT4, True) # Back right
 motor5 = Motor(Ports.PORT5, False)
 motor6 = Motor(Ports.PORT6, True)
-motorgroupleft = MotorGroup(motor1, motor3, motor5)
-motorgroupright = MotorGroup(motor2, motor4, motor6)
+motorgroupleft = MotorGroup(motor1, motor3)
+motorgroupright = MotorGroup(motor2, motor4)
 drivemode = DriveTrain(motorgroupleft, motorgroupright, 319.19, 295, 40, MM, 1)
 escalator = Motor(Ports.PORT5, True)
 #bing bong
 def autonomous():
     return
 def drive():
-    if (abs(controller.axis1.position()))>5:
-        forward=-controller.axis1.position()
-    else:
-        forward=0
-    if(abs(controller.axis3.position())>5):
-        turn=controller.axis3.position()
-    else:
-        turn=0
-    if(abs(turn)+abs(forward))>100:
-        max=(abs(turn)+abs(forward))/100
-    else:
-        max=1
-    rightpower=(forward+turn)/max
-    leftpower=(forward-turn)/max
-    motorgroupleft.spin(leftpower)
-    motorgroupright.spin(rightpower)
+    motorgroupleft.spin(FORWARD, controller.axis3.value)
+    motorgroupright.spin(FORWARD, controller.axis2.value)
 def user_control():
     while True:
         drive()
